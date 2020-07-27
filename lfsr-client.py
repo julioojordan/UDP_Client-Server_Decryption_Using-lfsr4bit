9f3ada10e096e143e1d104f634efd5b60a35fa8e
Jordan <68832427+julioojordan@users.noreply.github.com>
2020-07-27 10:30:50 +0700

    Translatting Comments

--- lfsr-client.py
+++ lfsr-client.py
@@ -2,6 +2,8 @@
 
 #by Julio Andyan Jordan Aryanto
 #nim 24060117130078
+#DIPONEGORO UNIVERSITY
+# IT 17
 #client
 
 from socket import *
@@ -9,17 +11,17 @@ from socket import *
 serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('Masukan Cipherteks:')
-U = input('Masukan nilai U (4 bit): ')
+message = input('Masukan Cipherteks:') #input the cipherteks
+U = input('Masukan nilai U (4 bit): ') # input U
 
-#mengirimkan pesan ke server
+#mengirimkan pesan ke server / send a message to the server
 clientSocket.sendto(message.encode(),(serverName, serverPort))
 clientSocket.sendto(U.encode(),(serverName, serverPort))
 
 
-#mengambil pesan dari server
+#mengambil pesan dari server / take a message from the server
 message, serverAddress =clientSocket.recvfrom(2048)
 
-#menampilkan pesan
+#menampilkan pesan / showing the message
 print (message.decode())
 clientSocket.close()
