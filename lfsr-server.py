0328268c8c03c09b3fdaa98f19d9edbee6a3d6e8
Jordan <68832427+julioojordan@users.noreply.github.com>
2020-07-27 10:36:58 +0700

    Translatting Comments

--- lfsr-server.py
+++ lfsr-server.py
@@ -1,18 +1,20 @@
 #UDP Python with stream chiper decryption LFSR
 #by Julio Andyan Jordan Aryanto
 #nim 24060117130078
+#DIPONEGORO UNIVERSITY
+#IT 17
 #server
 
 from socket import *
 
-#mengubah array menjadi integer
+#mengubah array menjadi integer / change array to integer
 def array_int(array):
     array_int = []
     for i in range(len(array)):
         array_int.append(int(array[i]))
     return array_int
 
-#mengubah array menjadi string
+#mengubah array menjadi string / change arrays into strings
 def array_str(array):
     array_str = []
     for i in range(len(array)):
@@ -27,7 +29,7 @@ def listToString(array):
         str1 += array[i]
     return str1
 
-#mengubah biner ke decimal
+#mengubah biner ke decimal / change biner to decimal
 def BinaryToDecimal(binary):  
          
     binary1 = binary  
@@ -50,21 +52,21 @@ while 1:
     message, clientAddress = serverSocket.recvfrom(2048)
     U, clientAddress = serverSocket.recvfrom(2048)
 
-    #mengubah message menjadi str biner
+    #mengubah message menjadi str biner / change the message to str biner
     binMessage = ''.join(format(ord(i), 'b') for i in message.decode())
 
-    #mengubah binMassage menjadi array integer
+    #mengubah binMassage menjadi array integer / change binMessage to integer array
     binMessage_int = array_int(binMessage)
 
-    #mengubah U menjadi array integer
+    #mengubah U menjadi array integer / change U to integer array
     temp_keystream = U.decode()
     temp_keystream = array_int(temp_keystream)
     
-    #mengambil panjang pesan
+    #mengambil panjang pesan / take the message length
     panjang_pesan = len(binMessage)
 
-    #membuat keystream ide : LFSR 4 bit
-    keystream = [] #inisialisasi keystream
+    #membuat keystream ide : LFSR 4 bit / making the keystream of LFSR 4 bit
+    keystream = [] #inisialisasi keystream / initiate keystream
     for i in range(panjang_pesan):
         add = temp_keystream[3] ^ temp_keystream[0]
         temp_keystream[3] = temp_keystream[2]
@@ -76,35 +78,31 @@ while 1:
         keystream.append(add)
     keystream_str = array_str(keystream)
 
-    #melakukan proses dekripsi binMessage_int[i] XOR kestream [i]
+    #melakukan proses dekripsi binMessage_int[i] XOR kestream [i]/ decrypt binMessage_int [i] XOR kestream [i]
     plain = []
     for i in range(panjang_pesan):
         c = binMessage_int[i] ^ keystream[i]
         plain.append(c)
     plain_str = array_str(plain)
     
-    #mengubah list plain int menjadi list str
+    #mengubah list plain int menjadi list str/ change the plain int list to the str list
     lstr_plain = array_str(plain)
-    #list cipher str diubah ke dalam string dulu menggunakan
-    #fungsi listTostring
+    #list cipher str diubah ke dalam string dulu menggunakan fungsi listTostring / list cipher str is converted into a string first using the listTostring function
     str_plain = listToString(lstr_plain)
 
-    #mengubah ke dalam ASCII dan menyimpan ke cipherteks
+    #mengubah ke dalam ASCII dan menyimpan ke plainteks / change to ASCII and save to plaintext
     plainteks = ''
     for i in range(0, len(str_plain), 7): 
       
-        # memotong array cipher ke index range [0, 6] 
-        # dan menyimpannya ke temp_data 
+        # memotong array cipher ke index range [0, 6] dan menyimpannya ke temp_data
+        # cut the cipher array to index range [0, 6] and save it to temp_data
         temp_data = int(str_plain[i:i + 7]) 
        
-        # memasukan temp_data kefungsi binarytoDecimal 
+        # memasukan temp_data kefungsi binarytoDecimal / adding temp_data to binarytoDecimal function 
         decimal_data = BinaryToDecimal(temp_data) 
        
-        # Deccoding nilai decimal hasil dari  
-        # BinarytoDecimal() function, menggunakan chr()  
-        # function yang akan menghasilkan string yang sesuai  
-        # dengan karakter di tabel ASCII dan menyimpan ke
-        # str_data 
+        # Decoding nilai decimal hasil dari BinarytoDecimal() function, menggunakan chr() function yang akan menghasilkan string yang sesuai dengan karakter di tabel ASCII dan menyimpan ke str_data 
+        # Decode the decimal value of the BinarytoDecimal () function, using the chr () function that will produce a string that matches the characters in the ASCII table and saves it to str_data
         plainteks = plainteks + chr(decimal_data)
 
     print ('==============Result==============')
